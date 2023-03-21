93aa0a23dbb897deb038cfe7c84b220ad701f08f
JaysonSey <lytlebhanks44@gmail.com>
2023-03-21 10:38:30 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,40 +7,22 @@
  */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 1, fib2 = 2, fib3;
-	unsigned long fib1_half1, fib1_half2,fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	int count = 0;
+	unsigned int fib1 = 1, fib2 = 2, fib3;
 
-	for (count = 0; count < 92; count++)
+	printf("%u, %u", fib1, fib2);
+	count += 2;
+
+	while (count < 98)
 	{
-		sum = fib1 + fib2;
-		printf("%du,", fib3);
+		fib3 = fib1 + fib2;
+		printf(", %u", fib3);
+		count++;
 		fib1 = fib2;
 		fib2 = fib3;
 	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
-		printf("%du, %du", half1, half2);
-		if (count != 98)
-			printf(',');
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
-	}
-	printf('\n');
+
+	printf("\n");
+
 	return (0);
 }
-
